51befb8ec15c38c540f7ca1275e410aebcde7ee8
Edward Shin <edshin7@gmail.com>
2018-07-24 11:14:44 -0700

    Admin, url, views updates
    Added ninja to some admin pages
    Added a few url routes for admin
    Added view methods for admin

--- apps/unchained_app/urls.py
+++ apps/unchained_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^admin$', views.adminLoginForm, name="adminLogin"),
+    url(r'^admin/products$', views.adminProducts, name="adminProducts"),
+    url(r'^admin/products/(?P<productId>\d+)$', views.adminProductById, name="adminProductId"),
+    url(r'^admin/products/(?P<productId>\d+)/edit$', views.adminEdit, name="adminEdit"),
+    url(r'^admin/products/(?P<productId>\d+)/delete$', views.adminDelete, name="adminDelete")
 ]
--- apps/unchained_app/views.py
+++ apps/unchained_app/views.py
@@ -5,3 +5,21 @@ import bcrypt
 # Create your views here.
 def index(request):
     return render(request, "unchained_app/index.html")
+
+def adminLoginForm(request):
+    return render(request, "/unchained/admin.html")
+
+def adminProducts(request):
+    return render(request, "/unchained/admin_products.html")
+
+def adminProductById(request):
+    return render(request, "/unchained/admin_product_id.html")
+
+def adminEdit(request, productId):
+    context = {
+        "productId": productId
+    }
+    return render(request, "/unchained/admin_editProduct.html", context)
+
+def adminDelete(request, productId):
+    return redirect("/admin/products")
