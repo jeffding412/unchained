759bb748c3007e35d4b4f301c1635189070bb74f
Edward Shin <edshin7@gmail.com>
2018-07-24 16:47:36 -0700

    Additional Admin backend and linkage
    Finished most or all of the routing for admin pages
    Finished most of the backend for admin pages

--- apps/unchained_app/urls.py
+++ apps/unchained_app/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-<<<<<<< HEAD
-    url(r'^admin$', views.adminLoginForm, name="adminLogin"),
+    url(r'^$', views.index, name="index"),
+    url(r'^loginOrRegister$', views.login_or_register),
+    url(r'^admin$', views.adminLoginForm, name="adminLoginForm"),
+    url(r'^admin/login$', views.adminLogin, name="adminLogin"),
     url(r'^admin/products$', views.adminProducts, name="adminProducts"),
     url(r'^admin/products/(?P<productId>\d+)$', views.adminProductById, name="adminProductId"),
     url(r'^admin/products/(?P<productId>\d+)/edit$', views.adminEdit, name="adminEdit"),
-    url(r'^admin/products/(?P<productId>\d+)/delete$', views.adminDelete, name="adminDelete")
-=======
-    url(r'^loginOrRegister$', views.login_or_register)
->>>>>>> origin/master
+    url(r'^admin/products/(?P<productId>\d+)/delete$', views.adminDelete, name="adminDelete"),
+    url(r'^admin/products/(?P<productId>\d+)/processEdit$', views.adminProcessEdit, name="adminProcessEdit"),
+    url(r'^logout$', views.logout, name="logout")
 ]
--- apps/unchained_app/views.py
+++ apps/unchained_app/views.py
@@ -7,25 +7,6 @@ from .models import User, Shipping, Product, Image, Offer
 def index(request):
     return render(request, "unchained_app/index.html")
 
-<<<<<<< HEAD
-def adminLoginForm(request):
-    return render(request, "/unchained/admin.html")
-
-def adminProducts(request):
-    return render(request, "/unchained/admin_products.html")
-
-def adminProductById(request):
-    return render(request, "/unchained/admin_product_id.html")
-
-def adminEdit(request, productId):
-    context = {
-        "productId": productId
-    }
-    return render(request, "/unchained/admin_editProduct.html", context)
-
-def adminDelete(request, productId):
-    return redirect("/admin/products")
-=======
 def login_or_register(request):
     user = User.objects.filter(email=request.POST['email'])
     request.session['errors'] = User.objects.validator(request.POST)
@@ -41,4 +22,97 @@ def login_or_register(request):
         User.objects.create(email=request.POST['email'],password_hash=pw_hash,first_name="",last_name="",rating=0,num_sold=0,isAdmin=False)
     
     return redirect('/')
->>>>>>> origin/master
+
+def adminLoginForm(request):
+    if "curUserId" in request.session["curUserId"]:
+        if User.objects.get(id=request.session["curUserId"]).isAdmin:
+            return redirect("/admin/products")
+        return redirect("/products")
+
+    return render(request, "/unchained/admin.html")
+
+def adminLogin(request):
+    if request.method != "POST":
+        return redirect("/")
+
+    errors = User.objects.validator_admin(request.POST)
+
+    if len(errors) > 0:
+        return redirect("/admin")
+
+    request.session["curUserId"] = User.objects.get(email=request.POST["email"]).id
+    return redirect("/admin/products")
+
+def adminProducts(request):
+    if "curUserId" not in request.session["curUserId"]:
+        return redirect("/")
+
+    context = {
+        "products": Product.objects.all()
+    }
+    
+    return render(request, "/unchained/admin_products.html", context)
+
+def adminProductById(request, productId):
+    if "curUserId" not in request.session["curUserId"]:
+        return redirect("/")
+    
+    product = Product.objects.get(id=productId)
+    seller = Seller.objects.get(id=product.seller_id)
+    context = {
+        "product": product,
+        "seller": seller,
+        "shipping": seller.shippings.first()
+    }
+
+    return render(request, "/unchained/admin_product_id.html")
+
+def adminEdit(request, productId):
+    if "curUserId" not in request.session["curUserId"]:
+        return redirect("/")
+
+    context = {
+        "product": Product.objects.get(id=productId)
+    }
+    return render(request, "/unchained/admin_editProduct.html", context)
+
+def adminProcessEdit(request, productId):
+    if request.method != "POST":
+        if User.objects.get(id=request.session["curUserId"]).isAdmin:
+            return redirect("/admin/products")
+        return redirect("/products")
+
+    errors = Product.objects.validator(request.POST)
+
+    if len(errors) > 0:
+        for tag, error in errors.items():
+            messages.error(request, error, extra_tages=tag)
+        return redirect("/admin/products/" + str(productId) + "/edit")
+
+    product = Product.objects.get(id=productId)
+    product.name = request.POST["name"]
+    product.brand = request.POST["brand"]
+    product.description = request.POST["description"]
+    product.category = request.POST["category"]
+    product.image = request.POST["image"]
+    product.save()
+
+    return redirect("/admin/products/" + str(productId))
+
+def adminDelete(request, productId):
+    if "curUserId" not in request.session["curUserId"]:
+        return redirect("/")
+
+    # make sure none admins can't access product deleting route
+    if not User.objects.get(id=request.session["curUserId"]).isAdmin:
+        return redirect("/logout")
+
+    delProduct = Product.objects.get(id=productId)
+    delProduct.delete()
+
+    return redirect("/admin/products")
+
+def logout(request):
+    request.session.clear()
+    redirect("/")
+
