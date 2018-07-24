aa6959d25e5065caccfaddeeae52fb832c17ea86
dingzhengfuaa <dingzhengfuaa@gmail.com>
2018-07-24 10:40:47 -0700

    finished login/registration
    created email regex, finished login/registration validations in model.py, added error messages to index.html

--- apps/unchained_app/models.py
+++ apps/unchained_app/models.py
@@ -3,17 +3,33 @@ from django.db import models
 import re
 import bcrypt
 
+EMAIL_REGEX = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
 # Create your models here.
 class UserManager(models.Manager):
     def validator(self, postData):
         errors = {}
+        if len(postData['email']) < 1:
+            errors["email"] = "User email is a required field"
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors["email"] = "User email is not in valid format"
+
+        if len(postData['password']) < 8:
+            errors["password"] = "Password should be at least 8 characters"
+
+        user = User.objects.filter(email=postData['email'])
+        if not user:
+            errors['failure'] = "No email/password combo"
+        elif not bcrypt.checkpw(postData['password'].encode(), user[0].password_hash.encode()):
+            errors['failure'] = "No email/password combo"
+
         return errors
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+    password_hash = models.CharField(max_length=255)
     rating = models.FloatField()
     num_sold = models.IntegerField()
     isAdmin = models.BooleanField()
--- apps/unchained_app/urls.py
+++ apps/unchained_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^loginOrRegister$', views.login_or_register)
 ]
